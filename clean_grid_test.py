1de76c95256eced47763f30c31c3ef1c3664a3f1
ahangchen <1418659400@qq.com>
2017-03-13 16:30:07 +0800

    clean grid test no label imgs

---
+++ clean_grid_test.py
@@ -0,0 +1,17 @@
+from file_helper import read_lines, write_line
+
+
+def clean_grid():
+    grid_test_path = 'top-m2g-std4-test/test_track.txt'
+    new_test_path = 'top-m2g-std4-test/test_tracks.txt'
+    grid_test_lines = read_lines(grid_test_path)
+    for i, grid_test_line in enumerate(grid_test_lines):
+        tail = grid_test_line[4: -1]
+        if i < 775:
+            write_line(new_test_path, ('%04d' % (i + 251)) + tail)
+        else:
+            write_line(new_test_path, grid_test_line[0: -1])
+
+
+if __name__ == '__main__':
+    clean_grid()
