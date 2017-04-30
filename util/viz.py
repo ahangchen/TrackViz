87c79bc49666cb4ef21819ca57679169fbbae5cb
ahangchen <cweihang@foxmail.com>
2017-04-30 20:16:01 +0800

    viz util

---
+++ util/viz.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def draw_line(y_s, x_s, y_label, x_label, title, line_color):
+    plt.subplots()
+    plt.plot(x_s, y_s, color=line_color)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.title(title)
+    plt.show()
